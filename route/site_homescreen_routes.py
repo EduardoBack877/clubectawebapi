a5773f39efa74cf056ecdd96101e6e3f573274ad
bruno <brunorabuske@gmail.com>
2026-03-25 19:15:59 -0300

    select cards de ambiente

--- route/site_homescreen_routes.py
+++ route/site_homescreen_routes.py
@@ -11,16 +11,12 @@ def get_ambientes(db=Depends(database_controller.get_db)):
 
     result = db.execute(text("""
         SELECT 
-            a.ambientes_uid,
-            a.nome,
-            a.descricao,
-            a.capacidade,
-            r.data_reserva
-        FROM ambiente a
-        LEFT JOIN reservas r 
-            ON r.ambientes_uid = a.ambientes_uid
-            AND r.status = 'confirmada'
-        WHERE a.isactive = 1
+            ambientes_uid,
+            nome,
+            descricao,
+            capacidade
+        FROM ambiente
+        WHERE isactive = 1
     """)).fetchall()
 
     ambientes_dict = {}
@@ -36,12 +32,9 @@ def get_ambientes(db=Depends(database_controller.get_db)):
                 "nome": data["nome"],
                 "desc": data["descricao"],
                 "capacidade": data["capacidade"],
-                "img": "https://picsum.photos/400/300",
-                "indisponiveis": []
+                "img": "https://picsum.photos/400/300"
             }
 
-        if data["data_reserva"]:
-            ambientes_dict[uid]["indisponiveis"].append(data["data_reserva"].day)
 
     return list(ambientes_dict.values())
 
